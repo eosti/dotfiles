86042219319a92085eb535e289e5cb98572f1439
eosti <reidsox1@gmail.com>
2021-12-03 23:27:47 -0500

    add a few exludes to blankline

--- nvim.lua
+++ nvim.lua
@@ -223,9 +223,12 @@ cmp.setup {
 --
 
 require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
     show_current_context = true,
     show_current_context_start = false,
+    buftype_exclude = {
+        'terminal',
+        'nofile'
+    },
 }
 
 --
